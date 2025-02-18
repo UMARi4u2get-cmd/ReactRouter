95550386a70364635d05654b27ed21f97464dcb4
UMARi4u2get-cmd <itsumar.i4u2get@gmail.com>
2025-02-18 23:37:44 +0500

    add some error handling in github component usig fetch api

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,4 +1,4 @@
-// import React, { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 function Github() {
@@ -15,19 +15,35 @@ function Github() {
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p4 text-3xl">
-      Github followers: {data.followers}
-      <img
-        src={data.avatar_url}
-        alt="Git profile picture"
-        width={300}
-        className="p-4 cursor-pointer"
-      />
+      {data.error ? (
+        <p className="text-red-500">Error: {data.error}</p>
+      ) : (
+        <>
+          Github followers: {data.followers}
+          <img
+            src={data.avatar_url}
+            alt="Git profile picture"
+            width={300}
+            className="p-4 cursor-pointer"
+          />
+        </>
+      )}
     </div>
   );
 }
 
 export default Github;
+
 export const githubInfoLoader = async () => {
-  const response = await fetch("https://api.github.com/users/hiteshchoudhary");
-  return response.json();
+  try {
+    const response = await fetch(
+      "https://api.github.com/users/hiteshchoudhary"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    return { error: error.message };
+  }
 };
